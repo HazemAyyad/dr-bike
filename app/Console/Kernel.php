5e65e0e5c0e126ea239c7dcc0c79643bcedee9ba
HazemAyyad <hazem1fadil@gmail.com>
2026-05-16 10:45:33 +0300

    test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('checks:send-due-reminders')->daily();
+        $schedule->command('checks:send-due-reminders')->everyMinute();
     }
 
     /**
