9acc29d2abc6e340127754280e6392bbf96e43c6
HazemAyyad <hazem1fadil@gmail.com>
2026-06-07 14:23:58 +0300

    editEmployee

--- app/Http/Controllers/API/EmployeeDetails.php
+++ app/Http/Controllers/API/EmployeeDetails.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\EmployeeDetailResource;
 use App\Mail\NewEmployeeAccountMail;
 use App\Models\EmployeeAttendance;
+use App\Models\EmployeeAttendanceScan;
 use App\Support\AttendanceScanPresenter;
 use App\Models\EmployeeDetail;
 use App\Models\EmployeeOrder;
