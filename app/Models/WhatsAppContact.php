f8474c4442bf96d876e696b99eb35e8135f4d0d9
HazemAyyad <hazem1fadil@gmail.com>
2026-06-30 19:30:36 +0300

    edit

--- app/Models/WhatsAppContact.php
+++ app/Models/WhatsAppContact.php
@@ -11,8 +11,8 @@ class WhatsAppContact extends Model
     protected $fillable = ['name', 'phone', 'customer_id', 'supplier_id', 'employee_id', 'last_message_at'];
     protected $casts = ['last_message_at' => 'datetime'];
 
-    public function conversations() { return $this->hasMany(WhatsAppConversation::class); }
-    public function messages() { return $this->hasMany(WhatsAppMessage::class); }
+    public function conversations() { return $this->hasMany(WhatsAppConversation::class, 'whatsapp_contact_id'); }
+    public function messages() { return $this->hasMany(WhatsAppMessage::class, 'whatsapp_contact_id'); }
     public function customer() { return $this->belongsTo(Customer::class); }
     public function supplier() { return $this->belongsTo(Seller::class, 'supplier_id'); }
     public function employee() { return $this->belongsTo(User::class, 'employee_id'); }
--- app/Models/WhatsAppConversation.php
+++ app/Models/WhatsAppConversation.php
@@ -12,6 +12,6 @@ class WhatsAppConversation extends Model
     protected $casts = ['last_message_at' => 'datetime', 'unread_count' => 'integer'];
 
     public function contact() { return $this->belongsTo(WhatsAppContact::class, 'whatsapp_contact_id'); }
-    public function messages() { return $this->hasMany(WhatsAppMessage::class); }
+    public function messages() { return $this->hasMany(WhatsAppMessage::class, 'whatsapp_conversation_id'); }
     public function assignedAdmin() { return $this->belongsTo(User::class, 'assigned_admin_id'); }
 }
