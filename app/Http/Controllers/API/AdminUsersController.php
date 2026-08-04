fee3bdaae251e8282577bc6f9ecba61a9e50984d
HazemAyyad <hazem1fadil@gmail.com>
2026-08-04 14:31:00 +0300

    edit

--- app/Http/Controllers/API/AdminUsersController.php
+++ app/Http/Controllers/API/AdminUsersController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use App\Models\Logs;
 use App\Models\User;
 use App\Services\UserSessionManager;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
